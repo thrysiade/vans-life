06327223189aeb2b14926a8804428ed12731e2d3
thrysiade <90597817+thrysiade@users.noreply.github.com>
2024-01-19 20:09:04 +0800

    Update VanItem.jsx

--- components/VanItem.jsx
+++ components/VanItem.jsx
@@ -2,14 +2,19 @@ import { Link, useLocation } from "react-router-dom";
 
 const VanItem = ({van}) => {
     const { state } = useLocation();
+    const search = state?.search || ''
+    const type = state?.type || 'all'
 
     return ( 
         <div className="van-item">
             <Link 
-            to={state ? `..?${state.search}` : ".."}
+            // to={state ? `..?${state.search}` : ".."}
+            to={`..?${search}`}
             relative="path"
             >
-            <span>&larr;</span>{state.search ? `Back to all ${state.type} vans` : "Back to all vans"}</Link>
+            {/* <span>&larr;</span>{state.search ? `Back to all ${state.type} vans` : "Back to all vans"} */}
+            <span>&larr;</span>Back to {type} vans
+            </Link>
             <img src={van.imageUrl}></img>
             <p className="van-item-type">{van.type}</p>
             <h2>{van.name}</h2>
